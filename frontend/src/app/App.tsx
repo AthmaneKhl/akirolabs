95b94644cfc42cd839c34c516fb0b1514b3d1bfb
athmane <athm.kh@gmail.com>
2023-10-19 21:35:24 +0200

    fe: add infinite generation

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { DigitTable } from '../digit/DigitTable'
 import { getToken } from '../utils/generator'
 import { AppWrapper } from './app.style'
@@ -7,6 +7,19 @@ import { TokenList } from '../token/TokenList'
 function App() {
   const [selectedDigits, setSelectedDigits] = useState<number[]>([])
   const [tokens, setTokens] = useState<string[]>([])
+  const [infiniteGeneration, setInfiniteGeneration] = useState(false)
+  const intervalRef = useRef<number>(0)
+
+  useEffect(() => {
+    if (!infiniteGeneration) {
+      clearInterval(intervalRef.current)
+      return
+    }
+
+    intervalRef.current = setInterval(() => {
+      setTokens(tokens => [...tokens, getToken(selectedDigits)])
+    }, 1000)
+  }, [infiniteGeneration])
 
   const handleSelectedDigits = (digit: number) => {
     if (selectedDigits.includes(digit)) {
@@ -16,10 +29,13 @@ function App() {
     }
   }
 
+  const isDisabled = !selectedDigits.length
+
   return (
     <AppWrapper>
       <DigitTable selected={selectedDigits} onSelect={handleSelectedDigits} />
-      <button disabled={!selectedDigits.length && true} onClick={() => setTokens([...tokens, getToken(selectedDigits)])}>Generate</button>
+      <button disabled={isDisabled} onClick={() => setInfiniteGeneration(!infiniteGeneration)}>Generate indefinetely</button>
+      <button disabled={isDisabled} onClick={() => setTokens([...tokens, getToken(selectedDigits)])}>Generate</button>
       <TokenList tokens={tokens} />
 
     </AppWrapper>
