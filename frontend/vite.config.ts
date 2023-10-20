3c6660c0dcbe1a88e5d9c598742b430b0e0529af
athmane <athm.kh@gmail.com>
2023-10-20 17:56:40 +0200

    fe: switch port to 8080

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+const port = loadEnv("development", "./");
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: Number(port["VITE_PORT"]) ?? 8080,
+  },
+});
